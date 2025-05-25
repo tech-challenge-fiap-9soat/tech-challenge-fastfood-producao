c21e4ede0c5609689372248564f633eafe9b8d67
ItalloGomes <itallocoop@gmail.com>
2025-05-25 16:04:52 -0300

    fix: ajustes redis

--- src/main/java/com/techchallenge/fastfood/infrastructure/controller/PedidoController.java
+++ src/main/java/com/techchallenge/fastfood/infrastructure/controller/PedidoController.java
@@ -4,6 +4,7 @@ import com.techchallenge.fastfood.infrastructure.dto.PedidoDTO;
 import com.techchallenge.fastfood.infrastructure.enums.StatusPedido;
 import com.techchallenge.fastfood.usecases.pedido.PedidoCacheService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/pedido")
 public class PedidoController {
 
@@ -19,23 +21,32 @@ public class PedidoController {
 
     @PostMapping
     public void adicionar(@RequestBody @Valid PedidoDTO pedido) {
+        log.info("[POST] adicionando pedido - init...");
         pedidoCacheService.adicionarPedidoNaFila(pedido);
+        log.info("[POST] adicionando pedido - finished...");
     }
 
     @GetMapping
     public List<PedidoDTO> listar() {
-        return pedidoCacheService.listarFilaPedidos();
+        log.info("[GET] listar pedido - init...");
+        List<PedidoDTO> pedidos = pedidoCacheService.listarFilaPedidos();
+        log.info("[GET] listar pedido - finished...");
+        return pedidos;
     }
 
     @PatchMapping("/{id}/status/{statusPedido}")
     public ResponseEntity<Void> atualizarStatus(@PathVariable Long id, @PathVariable StatusPedido statusPedido) {
+        log.info("[PATCH] atualizarStatus pedido - init...");
         pedidoCacheService.atualizarStatusPedido(id, statusPedido);
+        log.info("[PATCH] atualizarStatus pedido - finished...");
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removerPedido(@PathVariable Long id) {
+        log.info("[DELETE] remover pedido - init...");
         pedidoCacheService.removerPedidoCache(id);
+        log.info("[DELETE] remover pedido - finished...");
         return ResponseEntity.ok().build();
     }
 
